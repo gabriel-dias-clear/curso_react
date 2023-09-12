ca2b3bbce82c8435878820866d24a6400419468d
gabriel-dias-clear <gabriel.dias.clear@gmail.com>
2023-09-12 07:59:00 -0300

    initial configs react

---
+++ fundamentos-react/src/App.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import ComParametro from './components/ComParametro';
+import Aleatorio from './components/Aleatorio';
+import IsPar from './components/isPar';
+import Card from './components/layout/Card';
+import ListaAlunos from './components/repeticao/ListaAlunos';
+import ListaProdutos from './components/repeticao/ListaProdutos';
+import Familia from './components/Familia';
+import './index.css'
+
+export default () => (
+    <div id='app'>
+        <h1>Fundamentos React</h1>
+    <div className='Cards'>
+    
+        <Card titulo="Exemplo de Card" color="#080">
+            <Aleatorio min={1} max={60}/>
+        </Card>
+        
+        <Card titulo="Exemplo de Card" color="#FAE428">
+            <ComParametro
+                titulo="Situação do Aluno"
+                aluno="Pedro Silva"
+            />
+        </Card>
+
+        <Card titulo="Exemplo de Card" color="#9E05CF">
+            <Familia></Familia>
+        </Card>
+        <Card titulo="Exemplo de Card" color="#FF4C65">
+            <ListaAlunos></ListaAlunos>
+        </Card>
+        <Card titulo="Exemplo de Card" color="#FF4C65">
+            <ListaProdutos/>
+        </Card>
+        <Card titulo="Exemplo de Card" color="#FF4C65">
+            <IsPar/>
+        </Card>
+    </div>
+    </div>
+)
---
+++ fundamentos-react/src/index.js
@@ -0,0 +1,10 @@
+import ReactDom from 'react-dom';
+import App from './App'
+
+const el = document.getElementById('root')
+
+ReactDom.render(
+    <>
+        <App></App>
+    </>, el
+)
