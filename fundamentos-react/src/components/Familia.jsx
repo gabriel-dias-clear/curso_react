e79099cf4e14ace7ab3e877aba1183fedfc77afc
gabriel-dias-clear <gabriel.dias.clear@gmail.com>
2023-09-12 07:59:29 -0300

    exercise family

---
+++ fundamentos-react/src/components/Familia.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FamiliaMembro from './FamiliaMembro';
+
+export default props =>{
+    return(
+        <div>
+            <FamiliaMembro nome="Gabriel"/>
+            <FamiliaMembro nome="Ana"/>
+            <FamiliaMembro nome="Maria"/>
+        </div>
+    )
+}
