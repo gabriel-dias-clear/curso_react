17acc9907386aff385661be5daba37a737402c8b
gabriel-dias-clear <gabriel.dias.clear@gmail.com>
2023-09-12 07:58:10 -0300

    first challenge

---
+++ fundamentos-react/src/components/ComParametro.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function ComParametro(props) {
+    return (
+        <>
+        
+        <h2> { props.titulo } </h2>
+        <h2> { props.aluno } </h2>
+        
+        </>
+    )
+}
---
+++ fundamentos-react/src/components/Primeiro.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Primeiro(){
+
+    return <h1>PRIMEIRO COMPONENTE</h1>
+
+}
