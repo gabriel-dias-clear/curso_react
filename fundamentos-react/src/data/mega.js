f1af1ffaa4d0635ec6a31b30ce1f576942ee4f92
gabriel-dias-clear <gabriel.dias.clear@gmail.com>
2023-09-13 09:35:51 -0300

    update mega method

--- fundamentos-react/src/data/mega.js
+++ fundamentos-react/src/data/mega.js
@@ -1,13 +1,12 @@
 function mega(){
-    const array = []
 
     function getRandomInt() {
         return Math.floor(Math.random() * (60-0) + 0);
       }
 
-
+      const array = []
  
-        for(var i = 0 ; array.length < 7; i++){
+        for(let i = 0 ; array.length < 7 ; i++){
             const randomNumber = getRandomInt()
                 
             if(!array.includes(randomNumber)){
