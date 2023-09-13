ffe023def5aebc0aef70c6410b482bb1c07c9875
gabriel-dias-clear <gabriel.dias.clear@gmail.com>
2023-09-13 10:52:53 -0300

    router content

--- router/src/components/layout/Content.jsx
+++ router/src/components/layout/Content.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
-import './Content.css'
-
-const Content = props => (
-
-    <main className='Content'>
-        componente content
+import  About  from "./../../views/examples/About";
+import  Home  from "./../../views/examples/Home";
+import { Routes, Route } from "react-router-dom";
+ 
+const Content = props => {
+  return (
+    <main className="content">
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
     </main>
-
-)
+  );
+};
 
 export default Content
