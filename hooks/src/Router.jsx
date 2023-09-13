a2469f9c15064dedc486c66315665652a6162727
gabriel-dias-clear <gabriel.dias.clear@gmail.com>
2023-09-13 12:44:21 -0300

    routes file

---
+++ hooks/src/Router.jsx
@@ -0,0 +1,33 @@
+import React from "react"; 
+import {
+    BrowserRouter,
+    Routes,
+    Route
+} from 'react-router-dom';
+
+import Home from './../src/Views/Home/index'
+import State from './../src/Views/State/index'
+import Effect from './../src/Views/Effect/index'
+
+const Router = () => {
+  return (
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={<Home></Home>}
+          />
+           <Route
+            path="/state"
+            element={<State></State>}
+          />
+           <Route
+            path="/effect"
+            element={<Effect></Effect>}
+          />
+        </Routes>
+      </BrowserRouter>
+  )
+}
+
+export default Router;
